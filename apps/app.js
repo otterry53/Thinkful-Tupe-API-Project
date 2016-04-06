e685c2ab482d19ed0bf86609fae1027e5c22dca4
otterry53 <twlow88@gmail.com>
2016-04-06 18:55:42 -0400

    still trying to clear all and reset

--- apps/app.js
+++ apps/app.js
@@ -1,40 +1,50 @@
 //build a page with a simple search form that allows the user to search YouTube videos.
 //Based on search results, page will display thumbnail images of videos that match the search.
 
+//Declare the function:
+
+//function to get videos from the YouTube API
+function getVideos(addKeywords) {
+    var params = {
+        part: "snippet",
+        key: "AIzaSyBgEpIxrEBtyY88B-dPz_6NknNvA3IBlsk",
+        q: addKeywords,
+        maxResults: 8,
+        type: "video"
+    };
+    var url = 'https://www.googleapis.com/youtube/v3/search';
+    $.getJSON(url, params, function (data) {
+        if (data.items == 0) {
+            alert("No videos with your keywords found.");
+        }
+        showThumbnails(data.items);
+    });
+}
+// function to display the thumbnails, etc.
+function showThumbnails(videos) {
+    var videosFound = "";
+    $.each(videos, function (index, video) {
+        videosFound += "<li><p>" + video.snippet.title + "</p><a href='https://www.youtube.com/watch?v=" + video.id.videoId + "' target='_blank'><img src='" + video.snippet.thumbnails.default.url + "'/></a></li>";
+    });
+    $('#found-videos ul').html(videosFound);
+}
+
 
 $(function () {
     $("#keywords-input-form").submit(function (event) {
         event.preventDefault();
         var addKeywords = $("#add-keywords").val();
         getVideos(addKeywords);
-
     });
-
-    function getVideos(addKeywords) {
-        var params = {
-            part: "snippet",
-            key: "AIzaSyBgEpIxrEBtyY88B-dPz_6NknNvA3IBlsk",
-            q: addKeywords,
-            maxResults: 8,
-            type: "video"
-        };
-        var url = 'https://www.googleapis.com/youtube/v3/search';
-        $.getJSON(url, params, function (data) {
-            if (data.pageInfo.totalResults == 0) {
-                alert("No videos with your keywords found.");
-            }
-            showThumbnails(data.items);
-            $("#keywords-input-form").val();
-
-        });
-    }
-
-    function showThumbnails(videos) {
-        var videosFound = "";
-        $.each(videos, function (index, video) {
-            videosFound += "<li><p>" + video.snippet.title + "</p><a href='https://www.youtube.com/watch?v=" + video.id.videoId + "' target='_blank'><img src='" + video.snippet.thumbnails.default.url + "'/></a></li>";
-        });
-        $('#found-videos ul').html(videosFound);
-    }
-
 });
+
+//Function to how to empty the "keyword-input-form and/or #add-keywords
+/*$(".clearAll").on("click", function () {
+    emptyForm();
+});
+
+function emptyForm {
+    $('#keywords-input-form').empty();
+    $('#add-keywords').val();
+    $('#found-videos ul').empty();
+}*/
