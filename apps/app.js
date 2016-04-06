26697c3eac583555eed6fc4d978b8785e5b5ff35
otterry53 <twlow88@gmail.com>
2016-04-06 01:27:03 -0400

    thumbnails retrieved; can't clear

--- apps/app.js
+++ apps/app.js
@@ -4,34 +4,43 @@
 
 //http://i3.ytimg.com/vi/<insert-youtube-video-id-here>/default.jpg
 
-$(function () {
-$('#input-form').on("click", function (event) {
-event.preventDefault();
-getVideos = $('#addKeywordsValue').val();
-});
+$(document).ready(function () {
+    $("#keywords-input-form").submit(function (event) {
+        event.preventDefault();
+        getVideos($("#add-keywords").val());
 
-function getVideos(keywords) {
-var params = {
-part: "snippet",
-key: "AIzaSyBgEpIxrEBtyY88B-dPz_6NknNvA3IBlsk",
-q: keywords,
-r: 'json',
-type: "video"
-};
-url = 'https://www.googleapis.com/youtube/v3/search';
-$.getJSON(url, params, function (data) {
-/*if (data.Search == 0) {
-alert("No videos with your keywords found.");
-} else {*/
-showResults(data.items);
-});
-}
+    });
 
-function showResults(thumbs) {
-var videosfound = " ";
-$.each(thumbs, function (index, thumbs) {
-html += '<li></p>' + thumbs.snippet.title + " " + thumbs.snippetthumbnails.default.url + '</p></li>';
-});
-$('#search-results ul').html(videosfound);
-}
+    function getVideos(query) {
+        $.getJSON('https://www.googleapis.com/youtube/v3/search', {
+                part: "snippet",
+                key: "AIzaSyBgEpIxrEBtyY88B-dPz_6NknNvA3IBlsk",
+                q: query,
+                maxResults: 8,
+                type: "video"
+            },
+            function (data) {
+                if (data.pageInfo.totalResults == 0) {
+                    alert("No videos with your keywords found.");
+                }
+                showThumbnails(data.items);
+
+            }
+        );
+    }
+
+    function showThumbnails(videos) {
+        var videosFound = "";
+        $.each(videos, function (index, video) {
+            videosFound += "<li><p>" + video.snippet.title + "</p><a href='https://www.youtube.com/watch?v=" + video.id.videoId + "' target='_blank'><img src='" + video.snippet.thumbnails.default.url + "'/></a></li>";
+        });
+        $('#found-videos ul').html(videosFound);
+    }
+
+    $('.newSearch').click(function () {
+        $('#add-keywords').empty();
+        $('#found-videos').val();
+
+
+    });
 });
